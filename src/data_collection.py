2f9c4969b3231fece05b4b0783f1b1e1556df23b
Bharath J P V <84281655+Bharathjpv@users.noreply.github.com>
2026-01-25 15:57:29 +0530

    parameter file and dvc with parameter updated manually

--- src/data_collection.py
+++ src/data_collection.py
@@ -4,11 +4,16 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn.model_selection import train_test_split
+import yaml
+
+with open("params.yaml", 'r') as yaml_file:
+    config = yaml.safe_load(yaml_file)
+
+test_size=config['data_collection']['test_size']
 
 data = pd.read_csv('data/water_potability.csv')
 
-train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
-
+train_data, test_data = train_test_split(data, test_size=test_size)
 data_path = os.path.join('data', 'raw')
 
 os.makedirs(data_path, exist_ok=True)
--- src/model_building.py
+++ src/model_building.py
@@ -2,9 +2,15 @@ from re import X
 import pandas as pd
 import numpy as np
 import os
+import yaml
 
 import pickle
 from sklearn.ensemble import RandomForestClassifier
+
+with open("params.yaml", 'r') as yaml_file:
+    config = yaml.safe_load(yaml_file)
+
+n_estimators = config['model_building']['n_estimators']
 
 train_data = pd.read_csv(os.path.join('data', 'processed', 'train_processed.csv'))
 test_data = pd.read_csv(os.path.join('data', 'processed', 'test_processed.csv'))
@@ -12,7 +18,7 @@ test_data = pd.read_csv(os.path.join('data', 'processed', 'test_processed.csv'))
 X_train = train_data.drop('Potability', axis=1)
 y_train = train_data['Potability']
 
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
 clf.fit(X_train, y_train)
 
 pickle.dump(clf, open('model.pkl', 'wb'))
